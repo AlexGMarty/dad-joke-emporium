a3fb892238277d84273f7b5b85d5b014786da530
AlexGMarty <alex.g.marty@gmail.com>
2025-11-11 11:32:32 -0700

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/interview-proj-web_Alex-Marty/",
   plugins: [react()],
   css: {
     modules: {
